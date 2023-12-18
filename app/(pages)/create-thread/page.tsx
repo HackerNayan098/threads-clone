18415607cddc31c6f28e23faa8dcca04c82c7766
HackerNayan098 <nayanbhalerao9800@gmail.com>
2023-12-18 20:56:23 +0530

    basic changes

--- app/(pages)/create-thread/page.tsx
+++ app/(pages)/create-thread/page.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const page = () => {
-  return <div>create post page</div>;
+  return (
+    <div>
+      <h1 className="font-bold text-3xl">Create Thread</h1>
+      <section className="border-2 p-4 m-2">
+        <div className="w-full p-4 ">
+          <button> add Image +</button>
+        </div>
+        <textarea className="w-full outline-0 p-2 text-lg" rows={10}></textarea>
+      </section>
+    </div>
+  );
 };
 
 export default page;
