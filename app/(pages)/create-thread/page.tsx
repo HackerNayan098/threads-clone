ac39e4d3317ffd99a9534f0bb0eae90b5277f057
HackerNayan098 <nayanbhalerao9800@gmail.com>
2023-12-19 19:53:51 +0530

    create thread page added

--- app/(pages)/create-thread/page.tsx
+++ app/(pages)/create-thread/page.tsx
@@ -1,14 +1,27 @@
 import React from "react";
+import { MdOutlinePhoto } from "react-icons/md";
+import { IoVideocamOutline } from "react-icons/io5";
+import { GoPaperAirplane } from "react-icons/go";
 
 const page = () => {
   return (
     <div>
       <h1 className="font-bold text-3xl">Create Thread</h1>
       <section className="border-2 p-4 m-2">
-        <div className="w-full p-4 ">
-          <button> add Image +</button>
+        <textarea
+          className="w-full outline-0 p-2 rounded"
+          rows={10}
+          placeholder="What's in your mind ?"
+        ></textarea>
+        <div className="w-full p-4 bg-white rounded p-4 flex items-center justify-between">
+          <div className="flex items-center gap-4 text-[#9ca3af]">
+            <MdOutlinePhoto size={30} />
+            <IoVideocamOutline size={32} />
+          </div>
+          <div className="text-blue-500 bg-[#f5f5f5] p-2 rounded-xl">
+            <GoPaperAirplane className="w-7 h-7 ml-1" />
+          </div>
         </div>
-        <textarea className="w-full outline-0 p-2 text-lg" rows={10}></textarea>
       </section>
     </div>
   );
